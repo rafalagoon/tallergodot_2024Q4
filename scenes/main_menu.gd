cd0ba86863cc5c02b69eb9d022b3666977be62f6
todogodot <todogodot@electrojocs.com>
2024-11-25 20:09:20 +0100

    Sesión 2

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://game.tscn")
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
 	
 	
 	
---
+++ scenes/npc.gd
@@ -0,0 +1,5 @@
+extends CharacterBody3D
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	print("Hola, que tal?")
